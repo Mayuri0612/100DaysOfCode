a45ec15acc0394d3314fdd0b1120a24251b16616
Mayuri0612 <500060750@stu.upes.ac.in>
2020-09-16 20:57:35 +0530

    5.2

---
+++ Day5/2.py
@@ -0,0 +1,20 @@
+#EASY : Min-max sum
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the miniMaxSum function below.
+def miniMaxSum(arr):
+    x = sum(arr)
+    mini = x-(max(arr))
+    maxi = x-(min(arr))
+    print(mini, end = ' ')
+    print(maxi)
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
